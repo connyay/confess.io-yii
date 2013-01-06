41c50ad2b49ae75be763d242cdcbf09651736517
connyay <conn.hindley@gmail.com>
2013-01-06 13:38:30 -0600

    change default action to view confessions

--- protected/extensions/RESTFullYii/config/routes.php
+++ protected/extensions/RESTFullYii/config/routes.php
@@ -1,17 +1,20 @@
 <?php
   return array(
-        'api/<controller:\w+>'=>array('<controller>/restList', 'verb'=>'GET'),
-        'api/<controller:\w+>/<id:\w+>'=>array('<controller>/restView', 'verb'=>'GET'),
-        'api/<controller:\w+>/<id:\w+>/<var:\w*>'=>array('<controller>/restView', 'verb'=>'GET'),
-        'api/<controller:\w+>/<id:\w+>/<var:\w*>/<var2:\w*>'=>array('<controller>/restView', 'verb'=>'GET'),
-        
-        array('<controller>/restUpdate', 'pattern'=>'api/<controller:\w+>/<id:\d+>', 'verb'=>'PUT'),
-        array('<controller>/restUpdate', 'pattern'=>'api/<controller:\w+>/<var:\w+>/<id:\d+>', 'verb'=>'PUT'),
-        array('<controller>/restDelete', 'pattern'=>'api/<controller:\w+>/<id:\d+>', 'verb'=>'DELETE'),
-        array('<controller>/restCreate', 'pattern'=>'api/<controller:\w+>', 'verb'=>'POST'),
-        array('<controller>/restCreate', 'pattern'=>'api/<controller:\w+>/<id:\w+>', 'verb'=>'POST'),
+		'api/<controller:\w+>'=>array('<controller>/restList', 'verb'=>'GET'),
+		'api/<controller:\w+>/<id:\w+>'=>array('<controller>/restView', 'verb'=>'GET'),
+		'api/<controller:\w+>/<id:\w+>/<var:\w*>'=>array('<controller>/restView', 'verb'=>'GET'),
+		'api/<controller:\w+>/<id:\w+>/<var:\w*>/<var2:\w*>'=>array('<controller>/restView', 'verb'=>'GET'),
+		
+		array('<controller>/restUpdate', 'pattern'=>'api/<controller:\w+>/<id:\d+>', 'verb'=>'PUT'),
+		array('<controller>/restUpdate', 'pattern'=>'api/<controller:\w+>/<var:\w+>/<id:\d+>', 'verb'=>'PUT'),
+		array('<controller>/restDelete', 'pattern'=>'api/<controller:\w+>/<id:\d+>', 'verb'=>'DELETE'),
+		array('<controller>/restCreate', 'pattern'=>'api/<controller:\w+>', 'verb'=>'POST'),
+		array('<controller>/restCreate', 'pattern'=>'api/<controller:\w+>/<id:\w+>', 'verb'=>'POST'),
 
-        's/<id:\w+>'=>'confessions/view',
-        'confessions' => 'confessions/index',
-        'confessions/new' => 'confessions/create',
-      );
+		'n/new' => 'confessions/create',
+		'n/<id:\w+>'=>'confessions/view',
+		'ns' => 'confessions/index',
+		's/'=>'confessions/index',
+		's/<id:\w+>'=>'confessions/view',
+		'' => 'confessions/index',
+	  );
--- protected/extensions/foundation/widgets/FounBreadcrumbs.php
+++ protected/extensions/foundation/widgets/FounBreadcrumbs.php
@@ -23,44 +23,39 @@ class FounBreadcrumbs extends CBreadcrumbs
 		
 		echo CHtml::openTag("ul", $this->htmlOptions)."\n";
 		$links=array();
-		if($this->homeLink===null){
-			$links[]=CHtml::tag("li", array(), CHtml::link(Yii::t('zii','Home'),Yii::app()->homeUrl));
-		}
-		else if($this->homeLink!==false)
-			$links[]=$this->homeLink;
-        
-        $last = count($this->links);
-        $i = 0;
+
+		$last = count($this->links);
+		$i = 0;
 		$htmlOptions = array();
 		foreach($this->links as $label=>$url)
 		{
 			if(is_string($label) || is_array($url)){
-			    if(is_array($url)){
-			        if(isset($url["label"])){
-        			    $label =  $url["label"];
-                        unset($url["label"]);
-			        }
-			        if(isset($url["unavailable"])){
-			            if(isset($htmlOptions["class"])){
-                             $htmlOptions["class"] .= " unavailable";
-                        }else{
-                            $htmlOptions["class"] = "unavailable";
-                        }
-                        unset($url["unavailable"]);
-			        }
-                    if(isset($url["url"])) {
-                    	$url = $url["url"];
-                    }
-			    }
+				if(is_array($url)){
+					if(isset($url["label"])){
+						$label =  $url["label"];
+						unset($url["label"]);
+					}
+					if(isset($url["unavailable"])){
+						if(isset($htmlOptions["class"])){
+							 $htmlOptions["class"] .= " unavailable";
+						}else{
+							$htmlOptions["class"] = "unavailable";
+						}
+						unset($url["unavailable"]);
+					}
+					if(isset($url["url"])) {
+						$url = $url["url"];
+					}
+				}
 				$links[]=CHtml::tag("li", $htmlOptions, CHtml::link($this->encodeLabel ? CHtml::encode($label) : $label, $url));
 			}
 			else
 				$links[]=CHtml::tag("li", $htmlOptions, '<span>'.($this->encodeLabel ? CHtml::encode($url) : $url).'</span>');
-            $i++;
-		    $htmlOptions = array();
-            if($i == $last-1){
-                $htmlOptions["class"] = "current";    
-            }
+			$i++;
+			$htmlOptions = array();
+			if($i == $last-1){
+				$htmlOptions["class"] = "current";    
+			}
 		}
 		echo implode("\n",$links);
 		echo CHtml::closeTag("ul");
